4c577054603964e63f014ef3d28171f46180b798
brezx774-arch <brezx774@gmail.com>
2026-08-15 03:22:09 +0000

    Rename app to Cricket Royale

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.sixappeal.cricket',
-  appName: 'Six Appeal Cricket',
+  appName: 'Cricket Royale',
   webDir: 'dist'
 };
 
